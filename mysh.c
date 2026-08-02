e7833b1b6c4604220e2b469db3cda4c186236345
lucasho323 <lucas.ho332@gmail.com>
2026-08-02 11:26:32 +0800

    新增 > 輸出重導向:分身內 dup2 換 fd 1 並截斷 argv

--- mysh.c
+++ mysh.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/wait.h>
 
 #define MAXARGS 64
@@ -30,19 +31,37 @@ int main(void) {
         if (split(line, argv) == 0)
             continue;
 
-        if (strcmp(argv[0], "cd") == 0) {        // 攔截:本尊自己做
+        if (strcmp(argv[0], "cd") == 0) { // 攔截:本尊自己做
             if (argv[1] == NULL)
                 printf("cd: 缺少路徑\n");
             else if (chdir(argv[1]) != 0)
                 perror("cd");
-            continue;                            // 不 fork,回去讀下一行
+            continue;
         }
 
-        if (strcmp(argv[0], "exit") == 0)        // 攔截:本尊自己結束
+        if (strcmp(argv[0], "exit") == 0) // 攔截:本尊自己結束
             break;
 
         pid_t pid = fork();
         if (pid == 0) {
+            // 掃 argv 找 >:換水管 + 砍尾巴(只有分身做)
+            for (int i = 0; argv[i] != NULL; i++) {
+                if (strcmp(argv[i], ">") == 0) {
+                    if (argv[i + 1] == NULL) {
+                        printf("mysh: > 後面缺少檔名\n");
+                        return 1;
+                    }
+                    int fd = open(argv[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                    if (fd < 0) {
+                        perror("open");
+                        return 1;
+                    }
+                    dup2(fd, 1);
+                    close(fd);
+                    argv[i] = NULL; // 句號往前搬,> 和檔名都不會被 execvp 讀到
+                    break;
+                }
+            }
             execvp(argv[0], argv);
             printf("找不到指令: %s\n", argv[0]);
             return 1;
